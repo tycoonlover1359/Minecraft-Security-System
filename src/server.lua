02b739c964a9c28d881a71b9640cd9ccba10ea85
tycoonlover1359 <6766257+tycoonlover1359@users.noreply.github.com>
2020-12-29 15:15:58 -0800

    Removed Websocket.isOpen() check and added message broadcaster

--- src/server.lua
+++ src/server.lua
@@ -9,17 +9,13 @@ local websocket = http.websocket(websocket_url)
 local secretKey, publicKey = ecc.keypair(ecc.random.random())
 
 local function refreshWebsocket()
-    print("Refreshing websocket")
-    if websocket.isOpen() then websocket.close() end
+    websocket.close()
     websocket = http.websocket(websocket_url)
 end
 
 local function modemHandler()
     while true do
         local event, side, frequency, replyFrequency, message, distance = os.pullEvent("modem_message")
-        if not websocket.isOpen() then
-            refreshWebsocket()
-        end
         if message.action == "handshake" then
             modem.transmit(1, 1, publicKey)
         else
@@ -34,29 +30,20 @@ end
 
 local function websocketHandler()
     while true do
-        if not websocket.isOpen() then
-            refreshWebsocket()
-        end
         local message, isBinary = websocket.receive()
         if not isBinary then
             if message then
                 print("Message received from websocket: " .. message)
+                local message messageToTransmit = {
+                    ["payload"] = message,
+                    ["payload_signature"] = ecc.sign(secretKey, message)
+                }
+                print("Broadcasting message")
+                modem.transmit(channel, channel, messageToTransmit)
             else
                 print("Empty message received from websocket.")
             end
         end
-        -- local event, url, contents, binary = os.pullEvent({"websocket_message", "websocket_closed"})
-        -- if event == "websocket_closed" then
-        --     refreshWebsocket()
-        -- elseif event == "websocket_message" then
-        --     print("Received message from websocket: \n" .. contents)
-        --     local messageToTransmit = {
-        --         ["payload"] = contents,
-        --         ["payload_signature"] = ecc.sign(secretKey, payload),
-        --         ["public_key"] = publicKey
-        --     }
-        --     modem.transmit(channel, channel, messageToTransmit)
-        -- end
     end
 end
 
