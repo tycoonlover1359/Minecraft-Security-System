b467bb38e8bbb678c6baff77022198f8c1e34bff
Tycoonlover1359 <tycoonlover1359@protonmail.com>
2020-12-29 01:20:21 -0800

    Started websocket handling

--- src/server.lua
+++ src/server.lua
@@ -1,11 +1,20 @@
 os.loadAPI("json")
 
 local settings = json.decodeFromFile("settings.json")
-local websocket = http.websocket(settings[websocket_url])
+local channel = settings["channel"]
+local websocket_url = settings["websocket_url"]
+
+local websocket = http.websocket(websocket_url)
+
+local function refreshWebsocket()
+    if websocket.isOpen() then websocket.close() end
+    websocket = http.websocket(websocket_url)
+end
 
 local function modemHandler()
     while true do
         local event, side, frequency, replyFrequency, message, distance = os.pullEvent("modem_message")
+
     end
 end
 
@@ -13,9 +22,9 @@ local function websocketHandler()
     while true do
         local event, url, contents, binary = os.pullEvent({"websocket_message", "websocket_closed"})
         if event == "websocket_closed" then
-
+            
         elseif event == "websocket_message" then
-            print(contents)
+            modem.transmit(channel, channel, contents)
         end
     end
 end
