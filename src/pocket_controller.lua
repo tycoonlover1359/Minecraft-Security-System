985afe336fe97cff74f62114c48fc50bd33e11e1
Tycoonlover1359 <tycoonlover1359@protonmail.com>
2020-12-30 14:27:56 -0800

    Added initial pocket controler program

--- src/pocket_controller.lua
+++ src/pocket_controller.lua
@@ -1,1 +1,15 @@
 os.loadAPI("json.lua")
+os.loadAPI("touchpoint.lua")
+
+local t = touchpoint.new()
+
+t:add("button", nil, 2, 2, 5, 5, colors.red, colors.lime)
+
+t:draw()
+
+while true do
+    local event, p1 = t:handleEvents(os.pullEvent())
+    if event == "button_click" then
+        t:flash(p1)
+    end
+end
