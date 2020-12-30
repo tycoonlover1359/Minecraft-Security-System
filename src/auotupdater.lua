c2faca42e314b28f0ba4cbec63b8bfe9ff8d6553
Tycoonlover1359 <tycoonlover1359@protonmail.com>
2020-12-30 11:11:04 -0800

    Added autoupdater audomatically updating itself

--- src/auotupdater.lua
+++ src/auotupdater.lua
@@ -1,1 +1,13 @@
+local baseUrl = "https://gitlab.com/tycoonlover1359/Minecraft-Security-System/-/raw/main/src/"
+
+local function download(urlPath, filePath)
+    local request = http.get(baseUrl .. urlPath)
+    local file = fs.open(filePath)
+    file.write(request.readAll())
+    file.close()
+    request.close()
+end
+
+download("autoupdater.lua", "autoupdater.lua")
+
 shell.run("main.lua")
