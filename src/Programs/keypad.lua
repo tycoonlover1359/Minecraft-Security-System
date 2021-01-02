05f18878ee4aefa62eb597d58306592b2fa109e9
Tycoonlover1359 <tycoonlover1359@protonmail.com>
2021-01-01 17:09:56 -0800

    Added test keypad code

--- src/Programs/keypad.lua
+++ src/Programs/keypad.lua
@@ -1,2 +1,7 @@
 os.loadAPI("touchpoint.lua")
 os.loadAPI("json.lua")
+
+local settings = json.decodeFromFile("settings.json")
+local monitorSide = settings["monitor_side"]
+
+local keypadMonitor = touchpoint.new(peripheral.wrap("top"))
