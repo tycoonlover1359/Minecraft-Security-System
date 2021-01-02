16ca8a8d57b186be104ced31f6d818cf6ff5f76e
Tycoonlover1359 <tycoonlover1359@protonmail.com>
2021-01-01 17:26:16 -0800

    Added keypad initial startup

--- src/Programs/keypad.lua
+++ src/Programs/keypad.lua
@@ -2,6 +2,25 @@ os.loadAPI("touchpoint.lua")
 os.loadAPI("json.lua")
 
 local settings = json.decodeFromFile("settings.json")
-local monitorSide = settings["monitor_side"]
+local monitorSide = settings["keypad_monitor_side"]
+local exitButtonSide = settings["exit_button_side"]
+local reboot = false
 
-local keypadMonitor = touchpoint.new(peripheral.wrap("top"))
+if not monitorSide then
+    reboot = true
+    term.write("Enter Keypad Side or ID: ")
+    settings["keypad_monitor_side"] = read()
+end
+
+if not exitButtonSide then
+    reboot = true
+    term.write("Enter Exit Button Side or ID: ")
+    settings["exit_button_side"] = read()
+end
+
+if reboot then 
+    local file = fs.open("settings.json", "w")
+    file.write(json.encode(file))
+    file.close()
+    os.reboot() 
+end
